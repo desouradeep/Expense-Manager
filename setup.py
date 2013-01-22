2c764414749b66f0e30570e3692cd0f576b1f72c
Souradeep De <souradeep.2011@gmail.com>
2013-01-23 01:35:21 +0530

    -a

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(name='Expense-Manager',
       author_email='souradeep.2011@gmail.com',
       url='http://github.com/desouradeep/Expense-Manager',
       license = 'http://www.gnu.org/copyleft/gpl.html',
-      data_files=[('/usr/share/applications',['expense-manager.desktop']),
-	('/usr/bin',['expense-manager']),
-	('/usr/share/pixmaps',['icons/expense-manager.png']),
-	('/usr/share/expense-manager',['bugfix.py','clean_database.py','edit.py','main.py','newEntry.py','stats.py','sort_file.py','update_list.py','viewer.py'])]
+      data_files=[('/usr/share/applications',['expensemanager.desktop']),
+	('/usr/bin',['expensemanager']),
+	('/usr/share/pixmaps',['icons/expensemanager.png']),
+	('/usr/share/expensemanager',['bugfix.py','clean_database.py','edit.py','main.py','newEntry.py','stats.py','sort_file.py','update_list.py','viewer.py'])]
       
      )
